445b32bc7965e53b8f7c50446114fdba8cd3d1dc
Uzair Ahmed <160987011+Uzair-Ahmed-Dev@users.noreply.github.com>
2024-12-10 20:19:02 +0500

    Add files via upload

---
+++ src/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
+import Footer from "./Components/Footer";
+
+export default function Home() {
+  return (
+ <div>
+  <Header/>
+  <HeroSection/>
+  <Footer/>
+ </div>
+  );
+}
